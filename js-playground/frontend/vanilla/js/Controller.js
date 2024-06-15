74703b56708c939129de3170c5e1d7169210ed51
Dev-Yesung <yeasung67@gmail.com>
2024-06-15 18:52:44 +0900

    추천검색어 구현 완료

--- js-playground/frontend/vanilla/js/Controller.js
+++ js-playground/frontend/vanilla/js/Controller.js
@@ -1,13 +1,19 @@
+import { TabType } from "./views/TabView.js";
+
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView, tabView }) {
+  constructor(
+    store,
+    { searchFormView, searchResultView, tabView, keywordListView }
+  ) {
     console.log(tag);
     this.store = store;
 
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
     this.tabView = tabView;
+    this.keywordListView = keywordListView;
 
     this.subscribeViewEvents();
     this.render();
@@ -18,6 +24,9 @@ export default class Controller {
       .on("@submit", (event) => this.search(event.detail.value))
       .on("@reset", () => this.reset());
     this.tabView.on("@change", (event) => this.changeTab(event.detail.value));
+    this.keywordListView.on("@click", (event) =>
+      this.search(event.detail.value)
+    );
   }
 
   search(searchKeyword) {
@@ -42,12 +51,26 @@ export default class Controller {
 
   render() {
     if (this.store.searchKeyword.length > 0) {
-      this.tabView.hide();
-      this.searchResultView.show(this.store.searchResult);
-      return;
+      return this.renderSearchResult();
     }
 
     this.tabView.show(this.store.selectedTab);
+    if (this.store.selectedTab === TabType.KEYWORD) {
+      this.keywordListView.show(this.store.getKeywordList());
+    } else if (this.store.selectedTab === TabType.HISTORY) {
+      this.keywordListView.hide();
+    } else {
+      throw "사용할 수 없는 탭입니다.";
+    }
+
     this.searchResultView.hide();
+  }
+
+  renderSearchResult() {
+    this.searchFormView.show(this.store.searchKeyword);
+    this.tabView.hide();
+    this.keywordListView.hide();
+
+    this.searchResultView.show(this.store.searchResult);
   }
 }
--- js-playground/frontend/vanilla/js/main.js
+++ js-playground/frontend/vanilla/js/main.js
@@ -1,6 +1,7 @@
 import Controller from "./Controller.js";
 import Store from "./Store.js";
 import storage from "./storage.js";
+import KeywordListView from "./views/KeywordListView.js";
 import SearchFormView from "./views/SearchFormView.js";
 import SearchResultView from "./views/SearchResultView.js";
 import Tabview from "./views/TabView.js";
@@ -16,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     searchFormView: new SearchFormView(),
     searchResultView: new SearchResultView(),
     tabView: new Tabview(),
+    keywordListView: new KeywordListView(),
   };
 
   new Controller(store, views);
